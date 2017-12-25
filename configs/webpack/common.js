b27e210648d718593a23595230f2d610ff7abe1a
kreedz <kreedz@users.noreply.github.com>
2017-12-25 13:34:20 +0500

    webpack:  - add useBabel, useCache options to at loader

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -36,6 +36,10 @@ module.exports = {
                     },
                     {
                         loader: 'awesome-typescript-loader',
+                        options: {
+                            useBabel: true,
+                            useCache: true
+                        }
                     }
                 ],
             },
